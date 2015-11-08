7fd6474777aebb0945efba6ab8a82b3a3bc94744
valdrinkoshi <valdrinkoshi@gmail.com>
2015-11-07 18:44:01 -0800

    styles updated for store buttons

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -3,29 +3,3 @@
 Parse.Cloud.define("hello", function (request, response) {
   response.success("Hello world!");
 });
-var InviteReq = Parse.Object.extend("InviteRequest");
-Parse.Cloud.beforeSave("InviteRequest", function (request, response) {
-  if (!request.object.get("name") || !request.object.get("email") || !request.object.get("zipcode")) {
-    response.error('Must give name, email and zipcode to get an invite.');
-  } else {
-    var query = new Parse.Query(InviteReq);
-    query.equalTo("email", request.object.get("email"));
-    //search for email
-    query.first({
-      success: function (object) {
-        if (object) {
-          response.error(
-            JSON.stringify({
-              status: 200,
-              message: 'email already invited'
-            }));
-        } else {
-          response.success();
-        }
-      },
-      error: function (error) {
-        response.success();
-      }
-    });
-  }
-});
